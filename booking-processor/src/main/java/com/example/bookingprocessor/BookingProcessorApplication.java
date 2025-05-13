68b961b7086dfc7ff134d5b8021e7f007e2f3aeb
Avi <avishkar.motheelal11@gmail.com>
2025-05-13 21:04:35 +0200

    Init

---
+++ booking-processor/src/main/java/com/example/bookingprocessor/BookingProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingProcessorApplication.class, args);
+	}
+
+}
